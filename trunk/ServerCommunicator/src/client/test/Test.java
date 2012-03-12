1c0bddb48f1a9a9ac49d7cfb2450a2869de4e97e
donkamillo89 <donkamillo89@f3797396-c1e8-8a95-9c61-838a5187c139>
2012-03-12 12:05:02 +0000

    update dla biblioteki ServerCommunicator obsługujący dodatkowo zapytania o dostarczenie paczki i paczki przypisane do konkretnego kuriera

--- trunk/ServerCommunicator/src/client/test/Test.java
+++ trunk/ServerCommunicator/src/client/test/Test.java
@@ -2,25 +2,47 @@ package client.test;
 
 import client.communication.Client;
 import client.event.ThreadListResponseEvent;
+import client.event.ThreadLoginEvent;
 
 public class Test {
 
-	public static void main(String [] args)
+	public static void main(String [] args) throws Exception
 	{
 		Client c = Client.getInstance();
-		c.requestUnassignedParcels(new ThreadListResponseEvent(){
+		c.logIn("misio", "password", new ThreadLoginEvent(){
 
 			@Override
 			public void process() {
-				for (String [] s : list)
-					System.out.println(s[0]);
+				if (success)
+					System.out.println("logged in");
+				else
+				{
+					System.out.println ("not logged!!!");
+					System.exit(1);
+				}
 				
 			}
 
 			@Override
 			public void errorOccured() {
-				System.out.println("Error");
+				System.out.println("Error login");
 				
 			}});
+		Thread.sleep(1000);
+		/*c.requestAssignedToMeParcels(new ThreadListResponseEvent(){
+
+			@Override
+			public void process() {
+				for (String [] ss : list)
+					System.out.println(ss[0]);
+				
+			}
+
+			@Override
+			public void errorOccured() {
+				System.out.print("Error");
+				
+			}});*/
+		c.deliverParcel(1,null);
 	}
 }
