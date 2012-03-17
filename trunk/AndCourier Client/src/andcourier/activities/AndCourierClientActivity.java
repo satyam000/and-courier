921a7524a3d017646337db38897127698b7014d9
donkamillo89 <donkamillo89@f3797396-c1e8-8a95-9c61-838a5187c139>
2012-03-17 09:43:43 +0000

    Klient: - poprawiona wielowątkowość interfejsu - zapamiętywanie adresu - zapamiętywanie loginu - memu w oknie logowania pozwalające na zmianę adresu

--- trunk/AndCourier Client/src/andcourier/activities/AndCourierClientActivity.java
+++ trunk/AndCourier Client/src/andcourier/activities/AndCourierClientActivity.java
@@ -10,21 +10,34 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import client.communication.Client;
 
 public class AndCourierClientActivity extends Activity {
     
-	private final String FILENAME = "hostAddress";
+	private String FILENAME;
 	private EditText address = null;
+	private boolean loginCaller = false;
+	private Handler handler;
+	private ProgressDialog savingDial;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        FILENAME = this.getString(R.string.hostAddressFile);
+        loginCaller = this.getIntent().getBooleanExtra("loginCaller", false);
+        if (loginCaller)
+        	((TextView)findViewById(R.id.firstRunText)).setVisibility(View.INVISIBLE);
         address = (EditText)findViewById(R.id.textHostAddress);
+        handler = new Handler();
+        savingDial = new ProgressDialog(this);
+		savingDial.setCancelable(false);
+		savingDial.setMessage(getString(R.string.savingHost));
         try
         {
         	FileInputStream in = this.openFileInput(FILENAME);
@@ -43,8 +56,15 @@ public class AndCourierClientActivity extends Activity {
         	Toast.makeText(this, getResources().getString(R.string.failedReadAddress), Toast.LENGTH_SHORT).show();
         	return;
         }
-        
-        startActivity(new Intent(this, LoginActivity.class));
+        if (!loginCaller)
+        	startActivity(new Intent(this, LoginActivity.class));
+    }
+    
+    @Override
+    public void onPause()
+    {
+    	savingDial.dismiss();
+    	super.onPause();
     }
     
     public void saveClicked(View v)
@@ -56,30 +76,45 @@ public class AndCourierClientActivity extends Activity {
     	}
     	else
     	{
-			ProgressDialog savingDial = new ProgressDialog(this);
-			savingDial.setCancelable(false);
-			savingDial.setMessage(res.getString(R.string.savingHost));
+			
 			savingDial.show();
-    		Client.setHostAddress(address.getText().toString());
-    		Client c = Client.getInstance();
-    		if (c == null)
-    		{
-    			Toast.makeText(this, res.getString(R.string.incorrectAddress), Toast.LENGTH_SHORT).show();
-    		}
-    		else
-    		{
-		    	try
-		    	{
-			    	FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
-			    	fos.write(address.getText().toString().getBytes());
-			    	fos.close();
-			    	savingDial.hide();
-			    	startActivity(new Intent(this, LoginActivity.class));
-		    	}
-		    	catch(Exception e){
-		    		Toast.makeText(this, res.getString(R.string.failedSaveAddress), Toast.LENGTH_SHORT).show();
-		    	}
-    		}
+			new Thread(new Runnable()
+			{
+				public void run() {
+					if (!Client.testAddress(address.getText().toString()))
+					{
+						handler.post(new Runnable(){
+							public void run() {
+								savingDial.hide();
+								Toast.makeText(AndCourierClientActivity.this, AndCourierClientActivity.this.getResources().getString(R.string.incorrectAddress), Toast.LENGTH_SHORT).show();
+							}});
+					}
+					else
+					{
+						Client.setHostAddress(address.getText().toString());
+						try
+				    	{
+					    	FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+					    	fos.write(address.getText().toString().getBytes());
+					    	fos.close();
+					    	handler.post(new Runnable(){
+								public void run() {
+									savingDial.hide();
+									if (loginCaller)
+							    		AndCourierClientActivity.this.finish();
+							    	else
+							    		AndCourierClientActivity.this.startActivity(new Intent(AndCourierClientActivity.this, LoginActivity.class));
+								}});
+					    	
+				    	}
+				    	catch(Exception e){
+				    		handler.post(new Runnable(){
+								public void run() {
+									Toast.makeText(AndCourierClientActivity.this, AndCourierClientActivity.this.getString(R.string.failedSaveAddress), Toast.LENGTH_SHORT).show();
+								}});
+				    	}
+					}
+				}}).start();
     	}
     }
 }
