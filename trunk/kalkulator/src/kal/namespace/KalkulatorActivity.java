54fae8eed80159696e822e2e3a2a8f7f55664634
donkamillo89 <donkamillo89@f3797396-c1e8-8a95-9c61-838a5187c139>
2012-04-03 13:17:53 +0000

    andtutorial

---
+++ trunk/kalkulator/src/kal/namespace/KalkulatorActivity.java
@@ -0,0 +1,13 @@
+package kal.namespace;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KalkulatorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
