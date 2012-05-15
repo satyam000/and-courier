d3b372f8d6cc7fe79a33d5ccc64b055bca602445
donkamillo89 <donkamillo89@gmail.com>
2012-05-15 11:13:04 +0000

    bugfix w głównym oknie

---
+++ AndCourier Client/src/andcourier/activities/MainScreenActivity.java
@@ -0,0 +1,28 @@
+package andcourier.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainScreenActivity extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainscreen);
+	}
+	
+	public void showMyParcels(View v)
+	{
+		
+	}
+	
+	public void showAllParcels(View v)
+	{
+		
+	}
+	
+	public void showUnassigned(View v)
+	{
+		
+	}
+}
---
+++ AndCourier Client/src/client/communication/Client.java
@@ -0,0 +1,126 @@
+package client.communication;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+import client.communication.messages.AssignedToMeParcelsRequest;
+import client.communication.messages.ClientRequest;
+import client.communication.messages.DeliverParcelRequest;
+import client.communication.messages.LoginRequest;
+import client.communication.messages.UnassignedParcelsRequest;
+import client.event.ThreadEventProcessor;
+import client.event.ThreadListResponseEvent;
+import client.event.ThreadLoginEvent;
+
+public class Client {
+
+	private static int port = 666;
+	private Socket socket;
+	private volatile ClientRequest ongoingMessage = null;
+	
+	private static volatile Client instance = null;
+	
+	private static String hostAddress = "localhost";
+	
+	public static void setHostAddress(String address)
+	{
+		if (instance != null)
+		{
+			try
+			{
+				instance.socket.close();
+			}
+			catch(Exception e){}
+			instance = null;
+		}
+		hostAddress = address;
+	}
+	
+	public static String getHostAddress()
+	{
+		return hostAddress;
+	}
+	
+	public static boolean testAddress(String address)
+	{
+		try
+		{
+			Socket socket = new Socket();
+			SocketAddress sockaddr = new InetSocketAddress(address, port);
+			socket.connect(sockaddr, 4000);
+		}
+		catch(Exception e)
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	private Client() throws Exception
+	{
+		socket = new Socket();
+		SocketAddress sockaddr = new InetSocketAddress(hostAddress, port);
+		socket.connect(sockaddr, 4000);
+		socket.setSoTimeout(4000);
+		socket.setKeepAlive(true);
+	}
+	
+	public synchronized static Client getInstance()
+	{
+		if (instance == null || instance.socket.isClosed())
+		{
+			try
+			{
+				instance = new Client();
+			}
+			catch(Exception e){return null;}
+		}
+		return instance;
+	}
+	
+	public synchronized static Client resetInstance()
+	{
+		try
+		{
+			instance = new Client();
+		}
+		catch(Exception e)
+		{
+			instance = null;
+		}
+		return instance;
+	}
+	
+	public void logIn(String login, String password, ThreadLoginEvent eproc)
+	{
+			if (ongoingMessage != null && ongoingMessage.isAlive())
+				ongoingMessage.stop();
+			ongoingMessage = new LoginRequest(socket, eproc, login, password);
+			ongoingMessage.start();
+	}
+	
+	public void requestUnassignedParcels(ThreadListResponseEvent eproc)
+	{
+			if (ongoingMessage != null && ongoingMessage.isAlive())
+				ongoingMessage.stop();
+			ongoingMessage = new UnassignedParcelsRequest(socket, eproc);
+			ongoingMessage.start();
+	}
+	
+	public void requestAssignedToMeParcels(ThreadListResponseEvent eproc)
+	{
+			if (ongoingMessage != null && ongoingMessage.isAlive())
+				ongoingMessage.stop();
+			ongoingMessage = new AssignedToMeParcelsRequest(socket, eproc);
+			ongoingMessage.start();
+	}
+	
+	public void deliverParcel(int parcel_id, ThreadEventProcessor eproc)
+	{
+		if (ongoingMessage != null && ongoingMessage.isAlive())
+			ongoingMessage.stop();
+		ongoingMessage = new DeliverParcelRequest(socket, eproc, parcel_id);
+		ongoingMessage.start();
+	}
+}
